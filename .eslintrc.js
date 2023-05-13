485d9b440a57ae6aa74e665da7923aa145d2b0df
Vahin <danil.e.vakhrushev@gmail.com>
2023-05-14 01:04:35 +0500

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
- env: {
-  browser: true,
-  es2021: true
- },
- extends: [
-  'plugin:react/recommended',
-  'airbnb'
- ],
- parser: '@typescript-eslint/parser',
- parserOptions: {
-  project: ['tsconfig.json'],
-  tsconfigRootDir: __dirname,
-  ecmaFeatures: {
-    jsx: true
-  },
-  ecmaVersions: latest,
-  sourceType: 'module'
- },
- plugins: [
-  'react',
-  '@typescript-eslint'
- ],
- rules: {}
-}
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersions: latest,
+        sourceType: 'module'
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    rules: {}
+};
