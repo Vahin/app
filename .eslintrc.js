a0aae33923119fd85b4cb2bcf3aa40e21ad4aa01
Vahin <danil.e.vakhrushev@gmail.com>
2023-05-15 18:46:34 +0500

    Поправил конфигурационный файл для eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: ['tsconfig.json'],
-        tsconfigRootDir: __dirname,
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersions: latest,
-        sourceType: 'module'
-    },
-    plugins: [
-        'react',
-        '@typescript-eslint'
-    ],
-    rules: {}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
