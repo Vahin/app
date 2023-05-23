9dea9f208640e0a808be9ea4700b55ffed8e4f4d
Vahin <danil.e.vakhrushev@gmail.com>
2023-05-24 00:23:40 +0500

    Исправил buildPlugins для dev режима

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -13,22 +13,28 @@ import { BuildOptions } from './types/config';
 export const buildPlugins = ({
   paths,
   isDev,
-}: BuildOptions): WebpackPluginInstance[] => [
-  new HTMLWebpackPlugin({
-    template: paths.html,
-  }),
-  new ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new HotModuleReplacementPlugin(),
-  new ReactRefreshWebpackPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-    analyzerMode: 'static',
-  }),
-];
+}: BuildOptions): WebpackPluginInstance[] => {
+  const plugins = [
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+
+  return plugins;
+};
