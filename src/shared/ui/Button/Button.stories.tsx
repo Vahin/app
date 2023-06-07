8ddffff84ad75d18ea42574aab9cb306fd23f0fb
Vahin <danil.e.vakhrushev@gmail.com>
2023-06-08 00:48:31 +0500

    try to fix loki

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -40,7 +40,7 @@ export const OutlineSizeM: Story = {
   },
 };
 
-export const OutlineDis: Story = {
+export const OutlineDisabled: Story = {
   args: {
     ...OutlineSizeM.args,
     disabled: true,
