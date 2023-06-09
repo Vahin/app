b46c4cabf807188120490f57cff26ee23df7067f
Vahin <danil.e.vakhrushev@gmail.com>
2023-06-10 02:46:45 +0500

    Add tests for selector, async thunk and loginSlice

---
+++ src/features/authByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginError } from './getLoginError';
+
+describe('getLoginError.test', () => {
+  test('Возвращает корректное значение при наличии ошибки', () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: {
+        error: 'error',
+      },
+    };
+
+    expect(getLoginError(state as StateSchema)).toBe('error');
+  });
+
+  test('Возвращает корректное значение при отсутствии ошибки', () => {
+    const state: DeepPartial<StateSchema> = {};
+
+    expect(getLoginError(state as StateSchema)).toBe(undefined);
+  });
+});
---
+++ src/features/authByUsername/model/selectors/getLoginState/getLoginState.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginState } from './getLoginState';
+
+describe('getLoginState.test', () => {
+  test('Возвращает корректное значение при полном стэйте', () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: {
+        username: 'username',
+        password: '123123',
+        error: undefined,
+        isLoading: false,
+      },
+    };
+
+    expect(getLoginState(state as StateSchema)).toEqual(state.loginForm);
+  });
+
+  test('Возвращает корректное значение при пустом стэйте', () => {
+    const state: DeepPartial<StateSchema> = {};
+
+    expect(getLoginState(state as StateSchema)).toEqual(undefined);
+  });
+});
