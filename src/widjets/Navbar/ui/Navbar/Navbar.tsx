50ca77d66fad113af88c7265a974af80ab117650
Vahin <danil.e.vakhrushev@gmail.com>
2023-06-22 01:30:59 +0500

    Мемоизировал компонент навбара

--- src/widjets/Navbar/ui/Navbar/Navbar.tsx
+++ src/widjets/Navbar/ui/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useSelector } from 'react-redux';
 import { getNavbarItems } from 'widjets/Navbar/model/selectors/getNavbarItems';
+import { NavbarItemType } from 'widjets/Navbar/model/types/navbar';
 import cls from './Navbar.module.scss';
 import { NavbarItem } from '../NavbarItem/NavbarItem';
 
@@ -14,17 +15,17 @@ export const Navbar = memo((props: NavbarProps) => {
   const { className, collapsed = false } = props;
   const navbarItemsList = useSelector(getNavbarItems);
 
+  const itemsList = useMemo(() => navbarItemsList.map((item) => (
+    <NavbarItem
+      item={item}
+      collapsed={collapsed}
+      key={item.path}
+    />
+  )), [collapsed, navbarItemsList]);
+
   return (
     <nav className={classNames(cls.Navbar, {}, [className])}>
-      {
-        navbarItemsList.map((item) => (
-          <NavbarItem
-            item={item}
-            collapsed={collapsed}
-            key={item.path}
-          />
-        ))
-      }
+      {itemsList}
     </nav>
   );
 });
