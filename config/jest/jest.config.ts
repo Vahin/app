9e3348f7cb8f6b93e411aec12891e4f45eb18339
Vahin <danil.e.vakhrushev@gmail.com>
2023-07-28 02:12:36 +0500

    Добавил html-отчет для тестов

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -37,6 +37,15 @@ export default {
     __API__: '',
     __PROJECT__: 'jest',
   },
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: '<rootDir>/reports/unit',
+      filename: 'report.html',
+      openReport: true,
+      inlineSource: true,
+    }],
+  ],
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
