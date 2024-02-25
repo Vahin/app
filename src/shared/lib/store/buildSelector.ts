8f0a4bb7adbe7391455ac01f59696b4c92c6d70c
Vahin <danil.e.vakhrushev@gmail.com>
2024-02-25 15:28:07 +0500

    Добавил типизацию аргументов в buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,17 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
 
-export const buildSelector = <T>(selector: Selector<T>): Result<T> => {
-  const useSelectorHook = () => useSelector(selector);
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
+
+export const buildSelector = <T, Args extends any[]>(
+  selector: Selector<T, Args>,
+): Result<T, Args> => {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) =>
+    useSelector((state: StateSchema) => selector(state, ...args));
 
   return [useSelectorHook, selector];
 };
